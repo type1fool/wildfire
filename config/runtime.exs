b13350ed589dc4a48372bb468b61b419dacb71e3
Owen Bickford <owen@owencode.com>
2024-07-07 18:50:21 -0500

    rename api token to access token

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,12 +1,12 @@
 import Config
 
-config :wildfire, :fly, api_token: System.fetch_env!("FLY_API_TOKEN")
+config :wildfire, :fly, access_token: System.fetch_env!("FLY_ACCESS_TOKEN")
 
 # config/runtime.exs is executed for all environments, including
 # during releases. It is executed after compilation and before the
 # system starts, so it is typically used to load production configuration
 # and secrets from environment variables or elsewhere. Do not define
-# any compile-time configuration in here, as it won't be applied.
+# any compile-ti me configuration in here, as it won't be applied.
 # The block below contains prod specific runtime configuration.
 
 # ## Using releases
