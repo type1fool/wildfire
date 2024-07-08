e37da2310417a71b28069f3f0cdcc10306e2ef43
Owen Bickford <owen@owencode.com>
2024-07-07 19:43:21 -0500

    revert typo

--- config/runtime.exs
+++ config/runtime.exs
@@ -4,7 +4,7 @@ import Config
 # during releases. It is executed after compilation and before the
 # system starts, so it is typically used to load production configuration
 # and secrets from environment variables or elsewhere. Do not define
-# any compile-ti me configuration in here, as it won't be applied.
+# any compile-time configuration in here, as it won't be applied.
 # The block below contains prod specific runtime configuration.
 
 # ## Using releases
